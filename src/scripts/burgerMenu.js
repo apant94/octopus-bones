289b654b0c5324f5ec3806a72f356eec212c7a64
Anna Panteleeva <apanteleevadev@gmail.com>
2023-10-30 16:10:30 +0300

    fix: edit adaptive in index and care

--- src/scripts/burgerMenu.js
+++ src/scripts/burgerMenu.js
@@ -1,15 +1,20 @@
-const openBtn = document.querySelector('.header__burger-btn');
-const closeBtn = document.querySelector('.header__burger-close');
-const wrapper = document.querySelector('.header__burger-wrapper');
-const nav = wrapper.querySelector('.header__burger-nav');
-openBtn.addEventListener('click', () => {
-  wrapper.classList.add('opened');
-  nav.classList.remove('header__burger-nav_closed');
-  nav.classList.add('header__burger-nav_opened');
+const openBtn = document.querySelector(".header__burger-btn");
+const closeBtn = document.querySelector(".header__burger-close");
+const wrapper = document.querySelector(".header__burger-wrapper");
+const nav = wrapper.querySelector(".header__burger-nav");
+
+openBtn.addEventListener("click", () => {
+  wrapper.classList.add("opened");
+  nav.classList.remove("header__burger-nav_closed");
+  nav.classList.add("header__burger-nav_opened");
+  openBtn.style.visibility = "hidden";
+  closeBtn.style.visibility = "visible";
 });
 
-closeBtn.addEventListener('click', () => {
-  wrapper.classList.remove('opened');
-  nav.classList.remove('header__burger-nav_opened');
+closeBtn.addEventListener("click", () => {
+  wrapper.classList.remove("opened");
+  nav.classList.add("header__burger-nav_closed");
+  nav.classList.remove("header__burger-nav_opened");
+  openBtn.style.visibility = "visible";
+  closeBtn.style.visibility = "hidden";
 });
-
